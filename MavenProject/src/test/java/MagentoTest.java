472ad905bb0c7ca304ce5da11353e757a1865419
Lenovo <Lenovo@DESKTOP-7AKQI9H>
2019-01-22 15:22:53 +0530

    Second push

--- MavenProject/src/test/java/MagentoTest.java
+++ MavenProject/src/test/java/MagentoTest.java
@@ -2,14 +2,14 @@
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class MagentoTest {
 	@Test
 	public void magento()
 	{
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		String url = "http://www.magento.com";
